35eaea67f4da8f1eae24a9f513f4006c13ee05de
manojkokitkar21 <manojkokitkar21@gmail.com>
2023-03-24 11:02:23 +0530

    Added node api with JWT auth

---
+++ config/db.js
@@ -0,0 +1,5 @@
+module.exports ={
+    'secret' : "10xauthsecret",
+    //'db':"mongodb://localhost:27017/node-auth"
+    'db':'mongodb+srv://MK66:<password>@cluster0.ay6tqkd.mongodb.net/?retryWrites=true&w=majority'
+}
